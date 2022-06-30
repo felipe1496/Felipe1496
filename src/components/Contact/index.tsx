8929d51d2bb08c95ba3151af0863b731aefaf55c
felipe1496 <felipe.oliveira1496@gmail.com>
2022-06-29 22:09:26 -0300

    commit

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -6,35 +6,59 @@ import {
   Container,
   ContactContainer,
   MessageContainer,
-  InfoContainer,
+  SenderInfo,
+  MessageInfo,
 } from "./styles";
 
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [mail, setMail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [message, setMessage] = useState('');
+
   useEffect(() => {
     AOS.init({ duration: 2000 });
   });
+
+  const handleSubmit = () => {
+    console.log({
+      name,
+      mail,
+      phone,
+      message
+    })
+  }
+
   return (
     <Container id="contact">
+      <h1>
+        <img src="./assets/contact.svg" alt="" />
+        {" CONTACT ME"}
+      </h1>
       <ContactContainer>
-        <h1>
-          <img src="./assets/contact.svg" alt="" />
-          {" CONTACT ME"}
-        </h1>
         <MessageContainer>
-          <form>
-            <InfoContainer>
+          <form id="messageForm" onSubmit={(event) => {
+            event.preventDefault()
+            handleSubmit()
+          }}>
+            <SenderInfo>
               <label>Your Name</label>
-              <input type="text" />
+              <input type="text" onChange={(event) => setName(event.target.value)} />
 
               <label>Mail</label>
-              <input type="text" />
+              <input type="text" onChange={(event) => setMail(event.target.value)} />
 
               <label>Phone</label>
-              <input type="text" />
-            </InfoContainer>
-            <label>Message</label>
-            <input type="text" />
+              <input type="tel" onChange={(event) => setPhone(event.target.value)} />
+            </SenderInfo>
+            <MessageInfo>
+              <label>Message</label>
+              <textarea onChange={(event) => setMessage(event.target.value)} />
+            </MessageInfo>
           </form>
+          <button type="submit" form="messageForm">
+            Submit
+          </button>
         </MessageContainer>
       </ContactContainer>
     </Container>
--- src/components/Contact/styles.ts
+++ src/components/Contact/styles.ts
@@ -4,21 +4,27 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100vh;
+
   width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  padding-bottom: 1rem;
+  @media (min-width: 768px) {
+    display: flex;
+    height: 100vh;
+  }
+
+  @media (max-height: 550px) {
+    display: flex;
+    height: 100%;
+  }
 
   background: var(--light-blue);
   justify-content: center;
-`;
-
-export const ContactContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 80%;
-  margin-top: 80px;
-  border: 1px solid red;
-  width: 70%;
   h1 {
+    text-align: center;
+    width: 70%;
+    margin-top: 80px;
     font-family: "Rajdhani";
     color: var(--green);
     font-size: 2rem;
@@ -27,16 +33,91 @@ export const ContactContainer = styled.div`
       width: 20px;
     }
   }
+`;
 
-  @media (max-width: 900px) {
-    text-align: center;
-  }
+export const ContactContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80%;
+  width: 100%;
 `;
 
 export const MessageContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(1fr, 2);
-  border: 1px solid red;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  > form {
+    @media (min-width: 768px) {
+      display: flex;
+    }
+  }
+
+  @media (min-width: 768px) {
+    margin-top: 64px;
+    display: flex;
+    width: 50%;
+    min-width: 600px;
+    border-radius: 12px;
+    padding: 32px 0 32px 0;
+    background-color: var(--dark-blue);
+    max-width: 700px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    @media (max-height: 550px) {
+      background-color: transparent;
+      box-shadow: 0px 0px 0px;
+    }
+  }
+
+  > button {
+    font-size: 14px;
+    font-weight: 700;
+    color: white;
+    cursor: pointer;
+    background-color: var(--green);
+    border-radius: 12px;
+    margin-top: 16px;
+    width: 150px;
+    height: 40px;
+  }
 `;
 
-export const InfoContainer = styled.div``;
+export const SenderInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 2rem;
+  > label {
+    color: white;
+    font-size: 12px;
+  }
+  label:not(:first-child) {
+    margin-top: 10px;
+  }
+  > input {
+    width: 240px;
+    height: 40px;
+    background-color: transparent;
+    border: 1px solid #d7d9dc;
+    border-radius: 8px;
+    padding: 0 1rem 0 1rem;
+    color: white;
+  }
+`;
+
+export const MessageInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > label {
+    color: white;
+    font-size: 12px;
+  }
+  > textarea {
+    width: 240px;
+    height: 250px;
+    border-radius: 8px;
+    background-color: #d7d9dc;
+    padding: 1rem;
+  }
+`;
